aa451ebdeb119e0ba1dd21bf4e4c5da204690568
Swaminathan-MscIT <swamynathan.mscit@gmail.com>
2021-05-19 10:15:53 +0530

    UPDATED

---
+++ app/src/main/java/com/whysoseriouse/karthik/Services_offered.java
@@ -0,0 +1,19 @@
+package com.whysoseriouse.karthik;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class Services_offered extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.services_offered);
+
+    }
+}
